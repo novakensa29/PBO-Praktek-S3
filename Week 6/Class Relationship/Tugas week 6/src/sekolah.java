35dc7541c8e59c8017b85e4a363eaa7854c3430c
novakensa29 <91020796+novakensa29@users.noreply.github.com>
2021-10-22 12:36:46 +0700

    Done Week 6

---
+++ Week 6/Class Relationship/Tugas week 6/src/sekolah.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class sekolah {
+	private String Sekolah;
+	private String kepalaSekolah;
+	private List<kelas> kelas;
+	
+	public sekolah (String Sekolah, String kepalaSekolah, List<kelas> kelas) {
+		this.Sekolah = Sekolah;
+		this.kepalaSekolah = kepalaSekolah;
+		this.kelas = kelas;
+	}
+	
+	public void setSekolah(String Sekolah) {
+		this.Sekolah = Sekolah;
+	}
+	
+	public String getSekolah() {
+		return Sekolah;
+	}
+	
+	public void setkepalaSekolah(String kepalaSekolah) {
+		this.kepalaSekolah = kepalaSekolah;
+	}
+	
+	public String getkepalaSekolah() {
+		return kepalaSekolah;
+	}
+	
+	public List<kelas> getkelas(){
+		return kelas;
+	}
+	
+	public int getTotalMurid() {
+		int nomor = 0;
+		List<murid> murid;
+		for(kelas k:kelas) {
+			murid = k.getmurid();
+			for(murid m:murid) {
+				nomor++;
+			}
+		}
+		return nomor;
+	}
+	
+	public int getTotalGuru() {
+		int nomor = 0;
+		List<guru> guru;
+		for(kelas k:kelas) {
+			guru = k.getguru();
+			for(guru g:guru) {
+				nomor++;
+			}
+		}
+		return nomor;
+	}
+}
