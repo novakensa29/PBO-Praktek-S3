320eb99b047ec8cb4ff98fc692f8cc2706317670
novakensa29 <91020796+novakensa29@users.noreply.github.com>
2021-11-12 09:42:08 +0700

    Done Week 8

---
+++ Week 8/Soal 2/soal2/MethodOverloading.java
@@ -0,0 +1,15 @@
+package soal2;
+
+public class MethodOverloading {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Overload obj = new Overload();
+		double result;
+		obj.demo(10);
+		obj.demo(10,20);
+		result = obj.demo(5.5);
+		System.out.println("O/P :" + result);
+	}
+
+}
